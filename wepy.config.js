27cfa72169724127e24e11829fc8c8ec2b194904
Jas0ncn <ceocjy@vip.qq.com>
2017-04-07 17:15:53 +0800

    init project

---
+++ wepy.config.js
@@ -0,0 +1,57 @@
+var prod = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  wpyExt: '.wpy',
+  eslint: true,
+  compilers: {
+    less: {
+      compress: true
+    },
+    /* sass: {
+      outputStyle: 'compressed'
+    }, */
+    babel: {
+      sourceMap: true,
+      presets: [
+        'es2015',
+        'stage-1'
+      ],
+      plugins: [
+        'transform-export-extensions',
+        'syntax-export-extensions'
+      ]
+    }
+  },
+  plugins: {
+  }
+}
+
+if (prod) {
+
+  delete module.exports.compilers.babel.sourcesMap
+  // 压缩sass
+  // module.exports.compilers['sass'] = {outputStyle: 'compressed'}
+
+  // 压缩less
+  module.exports.compilers['less'] = {compress: true}
+
+  // 压缩js
+  module.exports.plugins = {
+    uglifyjs: {
+      filter: /\.js$/,
+      config: {
+      }
+    },
+    imagemin: {
+      filter: /\.(jpg|png|jpge)$/,
+      config: {
+        jpg: {
+          quality: 80
+        },
+        png: {
+          quality: 80
+        }
+      }
+    }
+  }
+}
