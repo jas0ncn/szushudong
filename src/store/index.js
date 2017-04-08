fcc0fb076bed6780636378484fe65ef5695381fb
Jas0ncn <ceocjy@vip.qq.com>
2017-04-08 16:37:27 +0800

    add new store to support wepy

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,21 @@
-import Store from './store'
+// import Store from './store'
+// To support wepy
+import deepclone from '../utils/deepclone'
+
+class Store {
+  constructor (state = {}) {
+    this.state = deepclone(state)
+  }
+
+  mutate (fn) {
+    this.state = deepclone(
+      fn(
+        deepclone(this.state)
+      )
+    )
+  }
+}
 
 module.exports = new Store({
-  userinfo: {},
   tab: 1
 })
