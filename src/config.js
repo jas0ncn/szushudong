5b322e4adb3270668673ea08af8d6977d0edd99b
Jas0ncn <ceocjy@vip.qq.com>
2017-04-09 13:26:31 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,18 @@
 const env = 'development' // 'development' or 'production'
+const hosts = {
+  development: 'http://localhost:3020',
+  production: 'https://sd.iszu.cn'
+}
 const api = {
-  list: {
-    method: 'GET',
-    url: '/blog/list'
-  },
+  /**
+   * login api
+   * need header:
+   * {
+   *   'x-wechat-code': code,
+   *   'x-wechat-encrypted': encryptedData,
+   *   'x-wechat-iv': iv
+   * }
+   */
   login: {
     method: 'POST',
     url: '/user/wxlogin'
@@ -11,18 +20,17 @@ const api = {
   userinfo: {
     method: 'GET',
     url: '/user/info'
+  },
+  list: {
+    method: 'GET',
+    url: '/blog/list'
   }
 }
 
-if (env === 'production') {
-  Object.keys(api).forEach(v => {
-    api[v].url = 'https://sd.iszu.cn' + api[v].url
-  })
-} else {
-  Object.keys(api).forEach(v => {
-    api[v].url = 'http://localhost:3020' + api[v].url
-  })
-}
+// add host name
+Object.keys(api).forEach(v => {
+  api[v].url = hosts[env] + api[v].url
+})
 
 module.exports = {
   env,
