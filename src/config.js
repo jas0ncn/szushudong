0fe78a300be9fb09807e5e8b9056f41087829e7c
Jas0ncn <ceocjy@vip.qq.com>
2017-04-10 02:49:35 +0800

    change api config

--- src/config.js
+++ src/config.js
@@ -1,42 +1,52 @@
+const disposeUrl = require('./utils/disposeUrl')
+
+// ENV
 const env = 'production' // 'development' or 'production'
+
+// development and production host
 const hosts = {
   development: 'http://localhost:3020',
   production: 'https://sd.iszu.cn'
 }
+
+// apis
 const api = {
-  /**
-   * login api
-   * need header:
-   * {
-   *   'x-wechat-code': code,
-   *   'x-wechat-encrypted': encryptedData,
-   *   'x-wechat-iv': iv
-   * }
-   */
-  login: {
-    method: 'POST',
-    url: '/user/wxlogin'
+  user: {
+    /**
+     * login api
+     * need header:
+     * {
+     *   'x-wechat-code': code,
+     *   'x-wechat-encrypted': encryptedData,
+     *   'x-wechat-iv': iv
+     * }
+     */
+    login: {
+      method: 'POST',
+      url: '/user/wxlogin'
+    },
+    info: {
+      method: 'GET',
+      url: '/user/info'
+    }
   },
-  userinfo: {
-    method: 'GET',
-    url: '/user/info'
-  },
-  list: {
-    method: 'GET',
-    url: '/blog/list'
-  },
-  like: {
-    method: 'POST',
-    url: '/blog/like'
+  blog: {
+    list: {
+      method: 'GET',
+      url: '/blog/list'
+    },
+    like: {
+      method: 'POST',
+      url: '/blog/like'
+    },
+    delete: {
+      method: 'POST',
+      url: '/blog/delete'
+    }
   }
 }
 
-// add host name
-Object.keys(api).forEach(v => {
-  api[v].url = hosts[env] + api[v].url
-})
-
 module.exports = {
   env,
-  api
+  api: disposeUrl(api, hosts[env])
 }
